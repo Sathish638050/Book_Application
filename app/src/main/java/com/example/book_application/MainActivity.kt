35bae7fb6553f4dc86eaa720a4576c22d7bd3caf
Administrator <sathish638050@gmail.com>
2021-12-08 10:44:36 +0530

    Book01

--- app/src/main/java/com/example/book_application/MainActivity.kt
+++ app/src/main/java/com/example/book_application/MainActivity.kt
@@ -10,6 +10,7 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.text.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,41 +24,46 @@ class MainActivity : AppCompatActivity() {
         val country = findViewById<TextInputLayout>(R.id.locationFilter)
         val filterBtn = findViewById<Button>(R.id.filterBtn)
         val resultCount = findViewById<TextView>(R.id.resultCount)
-        val listView = findViewById<ListView>(R.id.listView)
+        val listView = findViewById<TextView>(R.id.listView)
 
         filterBtn.setOnClickListener{
             var filtername  = name.editText?.text?.toString()
             var filtercountry = country.editText?.text?.toString()
-            var filterData :MutableList<String> = ArrayList()
-
+            var filterData : ArrayList<BookDetail> = ArrayList()
             if(postList != null){
-                for(Book in postList)
-                    if((!filtername.isNullOrEmpty()) && (!filtercountry.isNullOrEmpty())){
-                        if((filtername.toString() in Book.author.toString()) && (filtercountry.toString() in Book.country.toString())){
-                            filterData.add("Result: " + Book.title.toString())
-                        }
-                    }else if((filtername.isNullOrEmpty())){
-                        if(!(filtercountry.isNullOrEmpty())){
-                            if(filtercountry.toString() in Book.country.toString()){
-                                filterData.add("Result: "+Book.title.toString())
-                            }
-                        }
-                    }else if((filtercountry.isNullOrEmpty())){
-                        if(!(filtername.isNullOrEmpty())){
-                            if(filtername.toString() in Book.author.toString()){
-                                filterData.add("Result: "+Book.title.toString())
-                            }
+                for(Book in postList){
+                    if ((filtername != null) && (filtercountry != null)) {
+                        resultCount.visibility = View.VISIBLE
+                        if((filtername.isNotEmpty()) && (filtercountry.isNotEmpty())){
+                            filterData = postList.filter { it.author.toString() in filtername.toString() } as ArrayList<BookDetail>
+                        }else if(filtername.isNotEmpty()){
+                            filterData = postList.filter { it.author.toString() in filtername.toString() } as ArrayList<BookDetail>
+                        }else if(filtercountry.isNotEmpty()){
+                            filterData = postList.filter { filtercountry.toString() in it.country.toString() } as ArrayList<BookDetail>
                         }
                     }
+                }
             }
             if(filterData.size == 0){
-                resultCount.visibility = View.VISIBLE
-                resultCount.text = "Please enter author name and country"
-                listView.visibility = View.INVISIBLE
+                if((filtername != null) &&(filtercountry != null)){
+                    if((filtername.isNotEmpty())&&(filtercountry.isNotEmpty())){
+                        resultCount.visibility = View.VISIBLE
+                        resultCount.text = "Not Found"
+                        listView.visibility = View.INVISIBLE
+                    }else{
+                        resultCount.visibility = View.VISIBLE
+                        resultCount.text = "Please enter author name and country"
+                        listView.visibility = View.INVISIBLE
+                    }
+                }
             }else{
-                var adapter = ArrayAdapter<String>(applicationContext,android.R.layout.simple_list_item_1,filterData.take(3))
                 listView.visibility = View.VISIBLE
-                listView.adapter = adapter
+                var result = ""
+                val sb = StringBuilder()
+                for(book in filterData){
+                    sb.append("Result: "+book.title+"\n")
+                }
+                listView.setText(sb)
                 resultCount.visibility = View.VISIBLE
                 resultCount.text = "Result : " + filterData.size.toString()
             }
